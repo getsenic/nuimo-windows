5572e84607bd8e08490c185b60734b4558dd3aa8
René Simonsen <rss@knowhowdata.dk>
2016-10-03 20:40:14 +0200

    Changed reference to use placeholders for absolute paths instead of relative. Changed some AssemblyInfo strings back to their original values.

--- NuimoSDK/Properties/AssemblyInfo.cs
+++ NuimoSDK/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuimoSDK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Nuimo SDK")]
+[assembly: AssemblyDescription("Library for Windows 10 platforms to connect and communicate with Nuimo controllers made by Senic")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NuimoSDK")]
+[assembly: AssemblyCompany("Senic GmbH")]
+[assembly: AssemblyProduct("Nuimo SDK")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
